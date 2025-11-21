81ff6fc64d405019b85f7a5155e51f7a6608dbbe
mariathathiana <109003131+mariathathiana@users.noreply.github.com>
2025-11-21 12:34:34 +0100

    Firebase implementado

--- TripPlanner/ViewController.swift
+++ TripPlanner/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func signUp(_ sender: Any) {
+        
+        let email = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            print("User created account successfully")
+            
+        }
+        
+    }
+    @IBAction func signIn(_ sender: Any) {
+        let email = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            print("User signed in successfully")
+        }
+        
+    }
+    
 }
-
